5a263c0aded3be8dd006a50655b258d0f8f54d03
Shola Nur <shola.nur@gmail.com>
2023-02-02 13:08:35 -0800

    test2

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// This is exercise #1
 function giveMeSomething(a) {
 	return "something" + " " + a;
+}
+
+// Exercise #2
+function concat(name){
+    var b = "gmail.com"
+    var result = name + b;
+    return result;
 }
